b9ec9f25112d24fc382ed111a2edba5a0e3c7c92
Adam Bonsu <adam@bonsu.io>
2024-09-25 17:07:44 +0100

    refactor: Move terraform environment variable values into dev environment

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -1,16 +1,26 @@
 module "vpc" {
     source = "../../modules/vpc"
-
-    vpc_cidr = "10.0.0.0/16"
-    environment = "dev"
-    public_subnet_cidrs = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-    private_subnet_cidrs = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
     availability_zones = ["eu-west-2a", "eu-west-2b", "eu-west-2c"]
+    environment = "dev"
+    private_subnet_cidrs = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+    public_subnet_cidrs = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+    vpc_cidr = "10.0.0.0/16"
 }
 
 module "ecs" {
     source = "../../modules/ecs"
-
+    aws_ecs_cluster_name = "HelloWorldHexRubyWebRailsCluster20240924"
+    aws_ecs_service_desired_count = 3
+    aws_ecs_service_name = "HelloWorldHexRubyWebRailsService"
+    aws_ecs_service_lauch_type = "FARGATE"
+    aws_ecs_service_load_balancer_container_name = "HelloWorldHexRubyWebRailsContainerName"
+    aws_ecs_service_network_configuration_assign_public_ip = true
+    aws_ecs_task_definition_container_name = "HelloWorldHexRubyWebRailsCN"
+    aws_ecs_task_definition_family = "HelloWorldHexRubyWebRails"
+    aws_ecs_task_definition_memory = 2048
+    aws_lb_name = "HelloWorldHexRubyWebRailsLB"
+    aws_lb_target_group_name = "HelloWorldHexRubyWebRailsTG"
+    aws_security_group_port = 3000
     container_image = "757721680185.dkr.ecr.eu-west-2.amazonaws.com/hello-world-hex-ruby-web-rails:v4"
     execution_role_arn = "arn:aws:iam::757721680185:role/ecsTaskExecutionRole"
     public_subnet_ids = module.vpc.public_subnet_ids
@@ -20,7 +30,6 @@ module "ecs" {
 output "vpc_id" {
   description = "ID of the created VPC"
   value = module.vpc.vpc_id
-  
 }
 
 output "public_subnet_ids" {
--- terraform/modules/ecs/variables.tf
+++ terraform/modules/ecs/variables.tf
@@ -6,25 +6,21 @@ variable "vpc_id" {
 variable "aws_security_group_port" {
     description = "port number"
     type        = number
-    default     = 3000
 }
 
 variable "aws_ecs_cluster_name" {
     description = "Cluster Name"
     type        = string
-    default =   "HelloWorldHexRubyWebRailsCluster20240924"
 }
 
 variable "aws_ecs_task_definition_family" {
     description = "Task Definition Family"
     type        = string
-    default     = "HelloWorldHexRubyWebRails"
 }
 
 variable "aws_ecs_task_definition_container_name" {
     description = "Container Name"
     type        = string
-    default     = "HelloWorldHexRubyWebRailsCN"
 }
 
 variable "container_image" {
@@ -35,13 +31,11 @@ variable "container_image" {
 variable "execution_role_arn" {
     description = "Execution Role ARN"
     type        = string
-    default     = "arn:aws:iam::757721680185:role/ecsTaskExecutionRole"
 }
 
 variable "aws_lb_name" {
     description = "Load Balancer Name"
     type        = string
-    default     = "HelloWorldHexRubyWebRailsLB"
 }
   
 variable "public_subnet_ids" {
@@ -52,41 +46,34 @@ variable "public_subnet_ids" {
 variable "aws_lb_target_group_name" {
     description = "Target Group Name"
     type        = string
-    default     = "HelloWorldHexRubyWebRailsTG"
 }
 
 variable "aws_ecs_service_name" {
     description = "Service Name"
     type        = string
-    default     = "HelloWorldHexRubyWebRailsService"
 }
 
 variable "aws_ecs_service_desired_count" {
     description = "Desired Count"
     type        = number
-    default     = 3
 }
 
 variable "aws_ecs_service_lauch_type" { 
     description = "Launch Type"
     type        = string
-    default     = "FARGATE"
 }
 
 variable "aws_ecs_service_network_configuration_assign_public_ip" {
     description = "Assign Public IP"
     type        = bool
-    default     = true
 }
 
 variable "aws_ecs_service_load_balancer_container_name" { 
     description = "Container Name"
     type        = string
-    default     = "HelloWorldHexRubyWebRailsContainerName"
 }
 
 variable "aws_ecs_task_definition_memory" { 
     description = "Memory"
     type        = number
-    default     = 2048
 }
