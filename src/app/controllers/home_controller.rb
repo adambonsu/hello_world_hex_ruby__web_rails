e72ae2a98823ace069473b3f903626e7df22279f
Adam Bonsu <adam@bonsu.io>
2024-09-27 08:28:29 +0100

    serve html with initial tests

--- src/app/controllers/home_controller.rb
+++ src/app/controllers/home_controller.rb
@@ -3,7 +3,6 @@ class HomeController < ApplicationController
     def index
         user_interface = HelloWorldHexRuby::Adaptors::StringUserInterface.new
         core = HelloWorldHexRuby::Core::HelloWorldCore.new(user_interface)
-
-        render plain: (core.say_hello('from HelloWorldHexRuby::Core'))
+        @message = core.say_hello('from HelloWorldHexRuby::Core')
     end
 end
---
+++ src/test/controllers/home_controller_test.rb
@@ -0,0 +1,14 @@
+# test/controllers/home_controller_test.rb
+require 'test_helper'
+
+class HomeControllerTest < ActionDispatch::IntegrationTest
+    test "should get index" do
+        get home_index_url
+        assert_response :success
+    end
+
+    test "should display Hello World" do
+        get home_index_url
+        assert_select 'h1', 'Hello, from HelloWorldHexRuby::Core!'
+    end
+end
