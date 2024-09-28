038af507c995e457cdcb436f498486a955f1d5b4
Adam Bonsu <adam@bonsu.io>
2024-09-28 12:39:55 +0100

    testing ci-cd

--- src/app/controllers/home_controller.rb
+++ src/app/controllers/home_controller.rb
@@ -3,6 +3,6 @@ class HomeController < ApplicationController
     def index
         user_interface = HelloWorldHexRuby::Adaptors::StringUserInterface.new
         core = HelloWorldHexRuby::Core::HelloWorldCore.new(user_interface)
-        @message = core.say_hello('from HelloWorldHexRuby::Core')
+        @message = core.say_hello('from HelloWorldHexRuby::Core :)')
     end
 end
